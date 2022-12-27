0c7bb7f2e26b32a3b10fb82ebe3cbc0c007287d0
Tuğkan Ayhan <tugkanayhan@gmail.com>
2022-12-27 23:35:47 +0300

    Initial Commit

---
+++ RhythmAndCo/RhythmAndCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmAndCoApp.swift
+//  RhythmAndCo
+//
+//  Created by Tuğkan Ayhan on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RhythmAndCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
